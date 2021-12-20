370543dec9d8f68864b91381240276c69d843217
lelukeonfly@gmail.com <lelukeonfly@gmail.com>
2021-12-20 14:06:10 +0100

    first commita

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/master.css">
+    <title>secondhand</title>
+</head>
+<body>
+    
+    <div class="blog">
+        
+        <div class="header">
+            <h1>Secondhand</h1>
+        </div>
+
+        
+
+         <div class="blog-eintrag">
+
+            <h1>Titel</h1>
+            <p>content</p>
+
+        </div>
+
+        <div class="blog-eintrag">
+
+            <h1>Titel</h1>
+            <p>content</p>
+
+        </div>
+
+       <div class="blog-eintrag">
+
+            <h1>Titel</h1>
+            <p>content</p>
+
+        </div>
+
+        <div class="blog-eintrag">
+
+            <h1>Titel</h1>
+            <p>content</p>
+
+        </div>
+
+        <div class="login">
+            <form action="#" method="POST">
+                <input type="text" name="username" id="username" placeholder="username">
+                <input type="text" name="username" id="username" placeholder="password">
+                <input type="submit" value="login">
+            </form>
+        </div>
+
+
+
+    </div>
+
+</body>
+</html>
