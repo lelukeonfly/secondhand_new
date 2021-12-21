0389030a8d0d3d7c32e3de9608b899792dd23dca
lelukeonfly@gmail.com <lelukeonfly@gmail.com>
2021-12-21 16:25:25 +0100

    ausloggen copy pasta

--- includes/menu.inc.php
+++ includes/menu.inc.php
@@ -2,5 +2,5 @@
     <div><a href="#">Home</a></div>
     <div><a href="#">Neuer Eintrag</a></div>
     <div><a href="#">Benutzerverwaltung</a></div>
-    <div><a href="#">Ausloggen</a></div>
+    <div><a href="fw_ausloggen.php">Ausloggen</a></div>
 </div>
