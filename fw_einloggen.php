cc44a16294e6425fb68e1fc79a634c0f17bb0af2
lelukeonfly@gmail.com <lelukeonfly@gmail.com>
2021-12-21 16:09:30 +0100

    fixed but not fully fixed

--- fw_einloggen.php
+++ fw_einloggen.php
@@ -6,17 +6,18 @@ require_once 'includes/funktionen.inc.php';
     // Prüfe alle Benutzer, ob einer mit den übergebenen Daten übereinstimmt
         $db = get_db_connection();
         $fehler="";
+        var_dump($_POST);
         
-    if(empty(trim($_POST['benutzername'])) || empty(trim($_POST['passwort']))){
+    if(empty(trim($_POST['username'])) || empty(trim($_POST['password']))){
 
         $fehler = "Benutzername oder Passwort fehlt!";
 
     }else{
 
-        $abfrage = $db->query('SELECT * FROM personen b WHERE b.anmeldename ="'.$_POST['benutzername'].'" AND b.passwort = "'.$_POST['passwort'].'"');
+        $abfrage = $db->query('SELECT * FROM personen b WHERE b.anmeldename ="'.$_POST['username'].'" AND b.passwort = "'.$_POST['password'].'"');
         $result = $abfrage->fetch();
         
-        if($result['anmeldename'] === $_POST['benutzername'] && $result['passwort'] === $_POST['passwort']){
+        if($result['anmeldename'] === $_POST['username'] && $result['password'] === $_POST['passwort']){
 			logge_ein($result['anmeldename']);
         }else{
 
--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,7 +1,7 @@
 <div class="login">
     <form action="fw_einloggen.php" method="POST">
         <input type="text" name="username" id="username" placeholder="username">
-        <input type="password" name="username" id="username" placeholder="password">
+        <input type="password" name="password" id="username" placeholder="password">
         <input type="submit" value="login">
     </form>
 </div>
