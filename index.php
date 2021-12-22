8af03d1ee9b0de8cdd7063c0a851f2e6e67e3972
lelukeonfly@gmail.com <lelukeonfly@gmail.com>
2021-12-22 10:01:07 +0100

    cleaning code

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
     require_once 'includes/funktionen.inc.php';
     session_start();
     $blogeintraege = hole_eintraege();
-    #var_dump($_GET);
 ?>
 <!DOCTYPE html>
 <html lang="de">
@@ -20,56 +19,31 @@
         <div class="header">
             <h1>Secondhand</h1>
         </div>
+        
+
+        <?php
+        foreach($blogeintraege as $blogeintrag){
+        ?>
+        <div class="blog-eintrag">
+            <h1><?=$blogeintrag['bezeichnung'];?></h1>
+            <img src="<?=$blogeintrag['id'];?>.endung" alt="">
+            <p><?=$blogeintrag['beschreibung'];?></p>
+            <p>Preis: <?=$blogeintrag['preis'];?> €</p>
+            <p>Zustand: <?=$blogeintrag['name'];?></p>
+            <hr />
+            <p>erstellt am <?=date('d.m.Y H:i:s',strtotime($blogeintrag['erstelldatum']));?> von <?=$blogeintrag['vorname'];?> <?=$blogeintrag['nachname'];?></p>
+            <hr />
+            <h3 class="right">Kontaktdaten:</h3>
+            <p class="right">Email: <?=$blogeintrag['email'];?></p>
+            <p class="right">Telefon: <?=$blogeintrag['telefon'];?></p>
+        </div>
+        <?php
+        }
+        ?>
 
 
 
         <?php
-            foreach($blogeintraege as $key => $blogeintrag){
-                ?>
-                <div class="blog-eintrag">
-                <?php
-                foreach($blogeintrag as $key => $data){
-                    if(strcmp($key,"bezeichnung")==0){?>
-                        <h1><?=$data;?></h1><?php
-                    }elseif (strcmp($key,"id")==0) {?>
-                        <img src="<?=$data;?>.endung" alt=""><?php
-                    }
-                    elseif(strcmp($key,"beschreibung")==0){?>
-                        <p><?=$data;?></p><?php
-                    }elseif (strcmp($key,"preis")==0) {
-                    ?>
-                    <p>Preis: <?=$data;?> €</p>
-                    <?php
-                    }elseif (strcmp($key,"name")==0){
-                    ?>
-                    <p>Zustand: <?=$data;?></p>
-                    <?php
-                    }elseif(strcmp($key,"erstelldatum")==0||strcmp($key,"vorname")==0||strcmp($key,"nachname")==0||strcmp($key,"email")==0||strcmp($key,"telefon")==0){
-                    if(strcmp($key,"erstelldatum")==0){
-                        ?><hr />
-                        <p>erstellt am <?=date('d.m.Y H:i:s',strtotime($data));?><?php
-                    }elseif(strcmp($key,"vorname")==0){
-                        ?>
-                         von <?=$data;?>
-                        <?php
-                    }elseif(strcmp($key,"nachname")==0){
-                        ?>
-                         <?=$data;?></p><?php
-                    }elseif(strcmp($key,"email")==0){
-                        ?>
-                        <hr />
-                        <h3 class="right">Kontaktdaten:</h4>
-                        <p class="right">Email: <?=$data;?></p><?php
-                        }elseif(strcmp($key,"telefon")==0){
-                        ?>
-                        <p class="right">Telefon: <?=$data;?></p><?php
-                    }
-                }
-            }
-                ?>
-                </div>
-                <?php
-            }
 
                 /**
                  * Zeige das Login-Formular, wenn der Benutzer noch nicht eingeloggt ist,
